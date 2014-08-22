99788875c5500c7353c0afffc3a042d2a8fc180b
Maar10 <oudenniel@airfrance.fr>
2014-08-22 14:30:16 +0200

    version 0.0.1

---
+++ app.js
@@ -0,0 +1,8 @@
+(function (ng) {
+    'use strict';
+    ng.module('app', ['afkl.ng.lazyImage'])
+        .controller('mainCtrl', ['$scope', function mainCtrl($scope) {
+            // TODO generate url overhere for demo...
+
+        }]);
+})(angular);
