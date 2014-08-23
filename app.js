da665264b54db040ecce1ffccc35c77ec2ebce02
SquadraCorse <info@maas38.nl>
2014-08-24 00:34:23 +0200

    0.0.4

--- app.js
+++
@@ -1,8 +0,0 @@
-(function (ng) {
-    'use strict';
-    ng.module('app', ['afkl.lazyImage'])
-        .controller('mainCtrl', ['$scope', function mainCtrl($scope) {
-            // TODO generate url overhere for demo...
-
-        }]);
-})(angular);
