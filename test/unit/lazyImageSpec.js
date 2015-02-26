af4da61b06c0246b4a6322a9d11014a90a922cb4
SquadraCorse <info@maas38.nl>
2015-02-26 02:10:53 +0100

    alt test

--- test/unit/lazyImageSpec.js
+++ test/unit/lazyImageSpec.js
@@ -49,6 +49,14 @@ describe("Lazy image:", function() {
         expect(el[0].style.backgroundImage).toBeDefined();
     });
 
+    it('Do we have alt set on our image', function() {
+        var el = angular.element('<div afkl-lazy-image="img/foo.png" afkl-lazy-image-options=\'{"alt": "blue bird"}\'></div>');
+        $compile(el)(scope);
+        scope.$digest();
+
+        expect(el.html()).toBe('<img alt="blue bird" class="afkl-lazy-image" src="img/foo.png">');
+    });
+
     it('No image should be attached', function() {
         var el = angular.element('<div afkl-lazy-image=""></div>');
         $compile(el)(scope);
