c7483811e31f5d45c896c03a024c7fc4c7a151ab
SquadraCorse <info@maas38.nl>
2015-09-12 14:36:33 +0200

    directive inside repeat sample

--- sample/js/app.js
+++ sample/js/app.js
@@ -4,7 +4,8 @@
     .controller('appCtrl', ['$scope', function mainCtrl($scope) {
 
         $scope.count = 0;
-
+        $scope.searchText = 'a';
+        
         $scope.images = [
             {
                 srcset: '//placehold.it/480x240/00a1de/ffffff 480w, //placehold.it/768x384/00a1de/ffffff 768w',
@@ -12,18 +13,41 @@
             }
         ];
 
+        var changeImageUrl = function() {
+            var color = Math.floor(Math.random()*16777215).toString(16);
+            return '//placehold.it/768x599/' + color + '/ffffff';
+        }
+
         $scope.destroy = function() {
             $scope.images = null;
             $scope.$destroy();
         };
 
         $scope.changeImage = function () {
-            var color = Math.floor(Math.random()*16777215).toString(16);
-            $scope.runtimeImageSrc = '//placehold.it/768x599/' + color + '/ffffff';
+            $scope.runtimeImageSrc = changeImageUrl();
         }
 
-        $scope.changeImage();
+        $scope.collection = [{
+                srcset: changeImageUrl(),
+                type: 'a'
+            },{
+                srcset: changeImageUrl(),
+                type: 'a'
+            },{
+                srcset: changeImageUrl(),
+                type: 'a'
+            },{
+                srcset: changeImageUrl(),
+                type: 'b'
+            },{
+                srcset: changeImageUrl(),
+                type: 'a'
+            },{
+                srcset: changeImageUrl(),
+                type: 'b'
+        }];
 
+        $scope.changeImage();
 
     }])
     .directive('sampleCount', function () {
